1ec280ec8be709a6329cb180d03ac8fe102ebb2b
Alan Tan <alanwuha91@gmail.com>
2019-09-02 16:17:38 +0800

    lab 1 question 4

--- lab-1/lab1_transfer.m
+++ lab-1/lab1_transfer.m
@@ -10,7 +10,24 @@ NewImage = histTransfer(I1, I2);
 
 % Histogram transfer function
 function NewImage = histTransfer(I1, I2)
-    imshow(I2)
+    % Compute histogram of I1
+    I1_hist = imhist(I1);
     
-    NewImage = 0;
+    % Equalize I2 using I1's histrogram
+    NewImage = histeq(I2, I1_hist);
+    
+    figure(1);
+    imshow(I1);
+    figure(2);
+    imhist(I1);
+    
+    figure(3);
+    imshow(I2);
+    figure(4);
+    imhist(I2);
+    
+    figure(5);
+    imshow(NewImage);
+    figure(6);
+    imhist(NewImage);
 end
