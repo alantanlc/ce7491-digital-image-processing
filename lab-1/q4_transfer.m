9c8eb71e9bfcb122c969f0c7e92be534fd2b78f2
Alan <Alan@CSLS63.CSLS63>
2019-09-03 15:14:48 +0800

    lab 1

--- lab-1/q4_transfer.m
+++ lab-1/q4_transfer.m
@@ -16,18 +16,19 @@ function NewImage = histTransfer(I1, I2)
     % Equalize I2 using I1's histrogram
     NewImage = histeq(I2, I1_hist);
     
-%     figure(1);
-%     imshow(I1);
-%     figure(2);
-%     imhist(I1);
-%     
-%     figure(3);
-%     imshow(I2);
-%     figure(4);
-%     imhist(I2);
-%     
-%     figure(5);
-%     imshow(NewImage);
-%     figure(6);
-%     imhist(NewImage);
+    % Show figures
+    figure(1);
+    imshow(I1);
+    figure(2);
+    imhist(I1);
+    
+    figure(3);
+    imshow(I2);
+    figure(4);
+    imhist(I2);
+    
+    figure(5);
+    imshow(NewImage);
+    figure(6);
+    imhist(NewImage);
 end
