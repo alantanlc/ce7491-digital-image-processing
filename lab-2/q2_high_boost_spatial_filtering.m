7c4d4185786f83bd32df94f177c39bbbb6cf6b20
Alan Tan <alanwuha91@gmail.com>
2019-09-05 22:30:06 +0800

    lab 2 question 2

--- lab-2/q2_high_boost_spatial_filtering.m
+++ lab-2/q2_high_boost_spatial_filtering.m
@@ -5,13 +5,13 @@ clc; clear;
 I = imread('flower.tif');
 
 % Perform high boost filtering
-J = high_boost_filter(I, 2, 1);
+A = 1;
+mask = 2;
+J = high_boost_filter(I, mask, A);
 imshow([I, J]);
 
 % High-boost filtering function
 function J = high_boost_filter(I, M, A)
-    J = I;
-    
     % Verify that A is valid
     A = double(A);
     if A < 1
@@ -21,23 +21,26 @@ function J = high_boost_filter(I, M, A)
 
     % Generate mask
     if M == 1   % Mask 1
-        mask = double([0 -1 0; -1 A+4 -1; 0 -1 0]);
-        mask
+        mask = double([1 1 1; 1 -4 1; 1 1 1]);
     elseif M == 2   % Mask 2
-        mask = double([-1 -1 -1; -1 A+8 -1; -1 -1 -1]);
-        mask
+        mask = double([1 1 1; 1 -8 1; 1 1 1]);
     else
         'M must be 1 or 2'
+        return
     end
     
     % Convert I to double
     f = double(I);
     
-    % Perform convolution
+    % Compute Laplacian filtered image
+    L = zeros(size(I));
     for i=2:size(I,1)-1
        for j = 2:size(I,2)-1
            win = f(i-1:i+1, j-1:j+1);
-           J(i,j) = sum(win(:).*mask(:));
+           L(i,j) = sum(win(:).*mask(:));
        end
     end
+    
+    % Mix original image and Laplacian filtered image
+    J = (A-1).*f + L;
 end
