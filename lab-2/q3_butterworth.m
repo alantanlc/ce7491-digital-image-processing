ae02aa0286a93bc7b4ba5fda12fec3bb38b1ea1a
Alan Tan <alanwuha91@gmail.com>
2019-09-11 21:13:20 +0800

    lab 2 question 4

--- lab-2/q3_butterworth.m
+++ lab-2/q3_butterworth.m
@@ -1,7 +1,7 @@
 clear; clc;
 
 % Initialize variables
-d0 = 50
+d0 = 20;
 n = 2;
 ftype = 'low'; % low or high
 rows = 2;
@@ -30,7 +30,7 @@ function J = butterworth(I, d0, n, ftype)
     if strcmp(ftype, 'high') % high pass filter
         H = 1./(1+(d0^2./(X.^2+Y.^2)).^n);
     else % low pass filter
-        H = 1./(1+((X.^2+Y.^2)/d0^2).^n);
+        H = 1./(1.+((X.^2+Y.^2)/d0^2).^n);
     end
     subplot(rows,cols,6);
     fftshow(H);
@@ -50,7 +50,10 @@ function J = butterworth(I, d0, n, ftype)
     imshow(J);
 
     % Apply filter H to fftI plus original image
-    J = fftI + fftI.*H;
+    A = 0.5;
+    B = 0.5;
+    J = A.*(fftI) + B.*(fftI.*H);
+    
     subplot(rows,cols,8);
     fftshow(J);
     J = ifftshift(J);
@@ -58,33 +61,4 @@ function J = butterworth(I, d0, n, ftype)
     J = uint8(real(J(1:nx,1:ny)));
     subplot(rows,cols,4);
     imshow(J);
-end
-
-function fftshow(f,type)
-    % Usage: FFTSHOW(F,TYPE)
-    %
-    % Displays the fft matrix F using imshow, where TYPE must be one of
-    % 'abs' or 'log'. If TYPE='abs', then then abs(f) is displayed; if
-    % TYPE='log' then log(1+abs(f)) is displayed. If TYPE is omitted, then
-    % 'log' is chosen as a default.
-    %
-    % Example:
-    % c=imread('cameraman.tif');
-    % cf=fftshift(fft2(c));
-    % fftshow(cf,'abs')
-    %
-    if nargin<2,
-    type='log';
-    end
-    if (type=='log')
-    fl = log(1+abs(f));
-    fm = max(fl(:));
-    imshow(im2uint8(fl/fm))
-    elseif (type=='abs')
-    fa=abs(f);
-    fm=max(fa(:));
-    imshow(fa/fm)
-    else
-    error('TYPE must be abs or log.');
-    end
 end
---
+++ lab-2/q4_noise.m
@@ -0,0 +1,28 @@
+clear; clc;
+
+r = 1;
+c = 4;
+
+% Read in image
+I = imread('pepper_corrupt.tif');
+subplot(r,c,1);
+imshow(I,[]);
+
+% FFT
+I_fft = fftshift(fft2(I));
+subplot(r,c,2);
+fftshow(I_fft);
+
+% Apply butter bandpas
+L1 = butterlp(I, 35, 100);
+H = butterhp(I, 45, 100);
+B = L1 + H;
+J2 = B.*I_fft;
+J = B.*I_fft;
+subplot(r,c,3);
+fftshow(J2);
+
+% Show result
+K = ifft2(fftshift(J));
+subplot(r,c,4);
+imshow(K,[]);
