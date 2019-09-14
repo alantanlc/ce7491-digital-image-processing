be281c7c54961ba1004948563fc1dffde1273751
Alan Tan <alanwuha91@gmail.com>
2019-09-14 13:58:21 +0800

    lab 3 question 2

--- lab-3/q1_changing_colors.m
+++ lab-3/q1_changing_colors.m
@@ -13,20 +13,20 @@ I = imread('flowers.jpg');
 J = I;
 M = uint8(imread('mask.tif'));
 
-% Extract red and blue from image using mask
+% Part 1: Extract red and blue from image using mask
 M_red = I(:,:,1).*M;
 M_blue = I(:,:,3).*M;
 
-% Replace red values with blue values and vice versa
+% Part 1: Replace red values with blue values and vice versa
 J(:,:,1) = J(:,:,1) - M_red + M_blue;
 J(:,:,3) = J(:,:,3) - M_blue + M_red;
 
-% Convert RGB to HSV and extract hue using mask
+% Part 2: Convert RGB to HSV and extract hue using mask
 M = double(M);
 HSV = rgb2hsv(I);
 M_hue = HSV(:,:,1).*M;
 
-% Rotate M_hue by angles
+% Part 2: Rotate M_hue by angles
 angles = (0:40:360);
 for i=1:length(angles)
     H = mod(M_hue.*360+angles(i),360) / 360;
@@ -35,14 +35,14 @@ for i=1:length(angles)
     K(:,:,1,i) = K(:,:,1,i) - M_hue + H;
 end
 
-% Part 1 results
+% Part 1 Results
 figure(1);
 subplot(1,2,1);
 imshow(I,[]);
 subplot(1,2,2);
 imshow(J,[]);
 
-% Part 2 results
+% Part 2 Results
 figure(2);
 c = 4;
 r = ceil(length(angles)/c);
---
+++ lab-3/q2_histogram.m
@@ -0,0 +1,87 @@
+% Clear command window and workspace
+clear; clc;
+
+% Read in image
+I = imread('flowers.jpg');
+I_HSV = rgb2hsv(I);
+I_LAB = rgb2lab(I);
+
+% Part 1: Equalize the 3 RGB channels individually and combine the results
+RGB_1(:,:,1) = histeq(I(:,:,1));
+RGB_1(:,:,2) = histeq(I(:,:,2));
+RGB_1(:,:,3) = histeq(I(:,:,3));
+HSV_1 = rgb2hsv(RGB_1);
+LAB_1 = rgb2lab(RGB_1);
+
+% Part 2: Convert to HSV and equalize only V, convert back to RGB
+HSV_2 = rgb2hsv(I);
+HSV_2(:,:,3) = histeq(HSV_2(:,:,3));
+RGB_2 = hsv2rgb(HSV_2);
+LAB_2 = rgb2lab(RGB_2);
+
+% Part 3: Convert to L*a*b and equalize only L*, convert back to RGB
+LAB_3 = rgb2lab(I);
+LAB_3(1,:,:) = histeq(LAB_3(1,:,:));
+RGB_3 = lab2rgb(LAB_3);
+HSV_3 = rgb2hsv(RGB_3);
+
+% Results
+r = 2;
+c = 2;
+subplot(r,c,1);
+imshow(I);
+title('Original');
+subplot(r,c,2);
+imshow(RGB_1);
+title('(i)');
+subplot(r,c,3);
+imshow(RGB_2);
+title('(ii)');
+subplot(r,c,4);
+imshow(RGB_3);
+title('(iii)');
+
+% RGB Histogram
+figure; r = 4; c = 3;
+subplot(r,c,1); imhist(I(:,:,1)); title('Original R');
+subplot(r,c,2); imhist(I(:,:,2)); title('Original G');
+subplot(r,c,3); imhist(I(:,:,3)); title('Original B');
+subplot(r,c,4); imhist(RGB_1(:,:,1)); title('RGB R');
+subplot(r,c,5); imhist(RGB_1(:,:,2)); title('RGB G');
+subplot(r,c,6); imhist(RGB_1(:,:,3)); title('RGB B');
+subplot(r,c,7); imhist(RGB_2(:,:,1)); title('HSV R');
+subplot(r,c,8); imhist(RGB_2(:,:,2)); title('HSV G');
+subplot(r,c,9); imhist(RGB_2(:,:,3)); title('HSV B');
+subplot(r,c,10); imhist(RGB_3(:,:,1)); title('LAB R');
+subplot(r,c,11); imhist(RGB_3(:,:,2)); title('LAB G');
+subplot(r,c,12); imhist(RGB_3(:,:,3)); title('LAB B');
+
+% HSV Histogram
+figure; r = 4; c = 3;
+subplot(r,c,1); imhist(I_HSV(:,:,1)); title('Original H');
+subplot(r,c,2); imhist(I_HSV(:,:,2)); title('Original S');
+subplot(r,c,3); imhist(I_HSV(:,:,3)); title('Original V');
+subplot(r,c,4); imhist(HSV_1(:,:,1)); title('RGB H');
+subplot(r,c,5); imhist(HSV_1(:,:,2)); title('RGB S');
+subplot(r,c,6); imhist(HSV_1(:,:,3)); title('RGB V');
+subplot(r,c,7); imhist(HSV_2(:,:,1)); title('HSV H');
+subplot(r,c,8); imhist(HSV_2(:,:,2)); title('HSV S');
+subplot(r,c,9); imhist(HSV_2(:,:,3)); title('HSV V');
+subplot(r,c,10); imhist(HSV_3(:,:,1)); title('LAB H');
+subplot(r,c,11); imhist(HSV_3(:,:,2)); title('LAB S');
+subplot(r,c,12); imhist(HSV_3(:,:,3)); title('LAB V');
+
+% LAB Histogram
+figure; r = 4; c = 3;
+subplot(r,c,1); imhist(I_LAB(:,:,1)); title('Original L');
+subplot(r,c,2); imhist(I_LAB(:,:,2)); title('Original A');
+subplot(r,c,3); imhist(I_LAB(:,:,3)); title('Original B');
+subplot(r,c,4); imhist(LAB_1(:,:,1)); title('RGB L');
+subplot(r,c,5); imhist(LAB_1(:,:,2)); title('RGB A');
+subplot(r,c,6); imhist(LAB_1(:,:,3)); title('RGB B');
+subplot(r,c,7); imhist(LAB_2(:,:,1)); title('HSV L');
+subplot(r,c,8); imhist(LAB_2(:,:,2)); title('HSV A');
+subplot(r,c,9); imhist(LAB_2(:,:,3)); title('HSV B');
+subplot(r,c,10); imhist(LAB_3(:,:,1)); title('LAB L');
+subplot(r,c,11); imhist(LAB_3(:,:,2)); title('LAB A');
+subplot(r,c,12); imhist(LAB_3(:,:,3)); title('LAB B');
