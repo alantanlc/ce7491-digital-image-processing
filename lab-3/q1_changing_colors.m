40e8e484f21cd21060a51b24ee0a9de0ee768553
Alan Tan <alanwuha91@gmail.com>
2019-09-14 11:34:07 +0800

    lab 3 question 1

--- lab-3/q1_changing_colors.m
+++ lab-3/q1_changing_colors.m
@@ -1,15 +1,20 @@
+% Clear command window and workspace
 clear; clc;
 
+% Read in image and mask
 I = imread('flowers.jpg');
 J = I;
 M = uint8(imread('mask.tif'));
 
+% Extract red and blue from image using mask
 M_red = I(:,:,1).*M;
 M_blue = I(:,:,3).*M;
 
+% Replace red values with blue values and vice versa
 J(:,:,1) = J(:,:,1) - M_red + M_blue;
 J(:,:,3) = J(:,:,3) - M_blue + M_red;
 
+% Show result
 subplot(1,2,1);
 imshow(I,[]);
 subplot(1,2,2);
