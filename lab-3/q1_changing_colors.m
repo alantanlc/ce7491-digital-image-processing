e0df07741912c5d2a07130e2effb135472645933
Alan Tan <alanwuha91@gmail.com>
2019-09-14 13:06:14 +0800

    lab 3 question 1 part 2

--- lab-3/q1_changing_colors.m
+++ lab-3/q1_changing_colors.m
@@ -1,5 +1,5 @@
 % Clear command window and workspace
-clear; clc;
+clear; clc; clf;
 
 % Read in image and mask
 I = imread('flowers.jpg');
@@ -14,8 +14,33 @@ M_blue = I(:,:,3).*M;
 J(:,:,1) = J(:,:,1) - M_red + M_blue;
 J(:,:,3) = J(:,:,3) - M_blue + M_red;
 
-% Show result
+% Convert RGB to HSV and extract hue using mask
+M = double(M);
+HSV = rgb2hsv(I);
+M_hue = HSV(:,:,1).*M;
+
+% Rotate M_hue by angles
+angles = (0:40:360);
+for i=1:length(angles)
+    H = mod(M_hue.*360+angles(i),360) / 360;
+    H = H.*M;
+    K(:,:,:,i) = HSV;
+    K(:,:,1,i) = K(:,:,1,i) - M_hue + H;
+end
+
+% Part 1 results
+figure(1);
 subplot(1,2,1);
 imshow(I,[]);
 subplot(1,2,2);
 imshow(J,[]);
+
+% Part 2 results
+figure(2);
+c = 2;
+r = ceil(length(angles)/c);
+for i=1:length(angles)
+    subplot(r,c,i);
+    imshow(hsv2rgb(K(:,:,:,i)));
+    title(angles(i));
+end
