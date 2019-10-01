3e6661a222a0c12021924031bdd1824e13f869af
Alan Tan <alanwuha91@gmail.com>
2019-10-01 22:20:53 +0800

    lab 3 question 3 part 2

--- lab-3/q3_hough_transform.m
+++ lab-3/q3_hough_transform.m
@@ -1,28 +1,44 @@
-% Edge Detection generally involves:
-% 1. EDGE FILTERING - from an input image, generates a grayscale image
-% where pixels contain estimates of gradient magnitudes
-% 2. EDGEL DETECTION - creates a binary image containing edgels from the
-% edge filtered image
-% 3. EDGEL LINKING - produces a set of edges consisting of connected edgels
-
-% Clear command window and workspace
 clear; clc;
 
 % Read in image
 I = rgb2gray(imread('sniper.jpg'));
-[I_X, I_Y] = size(I);
+[r, c] = size(I);
+
+% Generate symmetrically reflected image
+R(r+1:2*r, c+1:2*c) = I;
+R(1:r, 1:c) = imrotate(I, 180);
+R(1:r, 2*c+1:3*c) = imrotate(I, 180);
+R(2*r+1:3*r, 1:c) = imrotate(I, 180);
+R(2*r+1:3*r, 2*c+1:3*c) = imrotate(I, 180);
+R(1:r, 1*c+1:2*c) = flip(I, 1);
+R(2*r+1:3*r, 1*c+1:2*c) = flip(I, 1);
+R(1*r+1:2*r, 1:c) = flip(I, 2);
+R(1*r+1:2*r, 2*c+1:3*c) = flip(I, 2);
+
+% Apply standard canny edge detection
+BW = edge(R, 'canny');
+BW = BW(r+1:2*r, c+1:2*c);
+
+% Apply hough transform
+[H,T,R] = hough(BW,'RhoResolution',0.5,'Theta',-90:0.5:89);
+
+% display the original image
+subplot(2,1,1);
 imshow(I);
+title('sniper.jpg');
 
-% Mesh
-[X,Y]=meshgrid(1:I_Y, 1:I_X);
-mesh(X,Y,I);
+% Display the hough matrix
+subplot(2,1,2);
+imshow(imadjust(rescale(H)),'XData',T,'YData',R,'InitialMagnification','fit');
+title('Hough transform of sniper.jpg');
+xlabel('\theta'), ylabel('\rho');
+axis on, axis normal, hold on;
+colormap(gca,hot);
 
-% Interactive value p and theta
-p = 1;
-theta = 1;
-
-% Edge filters
-gx_prewitt = [-1 -2 -1; 0 0 0; 1 2 1];
-gy_prewitt = [-1 0 1; -1 0 1; -1 0 1];
-gx_sobel = [-1 -2 -1; 0 0 0; 1 2 1];
-gy_sobel = [-1 0 1; -2 0 2; -1 0 1];
+% Find accumulator bin cell with largest number
+[p_max, p_ind] = max(H);
+[t_max, t_ind] = max(p_max);
+p_index = p_ind(t_ind);
+t_index = t_ind;
+p = R(p_index);
+t = T(t_index);
